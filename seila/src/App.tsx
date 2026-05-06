2cd44b8efcc757e062c4d724338cf510251872c1
Donathan Goncalves <donathanramalho@yahoo.com.br>
2026-05-06 17:17:11 -0300

    feat: sideBar

--- seila/src/App.tsx
+++ seila/src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
 import Home from './pages/home'
 import Products from './pages/products'
 
 import NavBar from './components/navBar'
+import SideBar from './components/sideBar'
 
 function App() {
-
+  const [sideBarOpen, setSideBarOpen] = useState(false)
   return (
     <>
-      <NavBar />
+      {sideBarOpen && <SideBar setSideBarOpen={setSideBarOpen} />}
+      <NavBar setSideBarOpen={setSideBarOpen} />
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="products/" element={<Products/>}></Route>
